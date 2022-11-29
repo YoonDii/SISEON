dd42262cd1a392bca398792acecc2ae3769dd511
hobin49 <hobin505@gmail.com>
2022-11-29 11:19:39 +0900

    feat/navbar navbar 구성

--- SS/urls.py
+++ SS/urls.py
@@ -14,10 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("", views.main),
+    path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls")),
     path("articles/", include("articles.urls")),
     path("gathering/", include("gathering.urls")),
