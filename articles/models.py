97b788b3128c1ad9b6675f6794ca65b1e8143e06
YoonDii <dbswl9506@naver.com>
2022-12-01 04:15:19 +0900

    feat/articles:md-ing

--- articles/models.py
+++ articles/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.conf import settings
 from imagekit.models import ProcessedImageField
 from imagekit.processors import ResizeToFill
+from simplemde.fields import SimpleMDEField
 
 # Create your models here.
 
@@ -20,7 +21,7 @@ class Articles(models.Model):
     )
     create_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    content = models.CharField(max_length=1000)
+    content = SimpleMDEField(verbose_name="mardown content")
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
@@ -31,13 +32,17 @@ class Articles(models.Model):
     unname = models.BooleanField(default=False)
     hits = models.PositiveIntegerField(default=0, verbose_name="조회수")
 
+
 class Photo(models.Model):
     article = models.ForeignKey(Articles, on_delete=models.CASCADE)
     image = models.ImageField(upload_to="images/", blank=True)
 
+
 class Comment(models.Model):
     content = models.TextField()
-    articles = models.ForeignKey(Articles, on_delete=models.CASCADE, related_name="comment_user")
+    articles = models.ForeignKey(
+        Articles, on_delete=models.CASCADE, related_name="comment_user"
+    )
     updated_at = models.DateTimeField(auto_now=True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     unname = models.BooleanField(default=False)
