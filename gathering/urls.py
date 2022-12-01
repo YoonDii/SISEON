cd6be3495fe70e16fd3767299fd4a2ea1703868f
hobin49 <hobin505@gmail.com>
2022-12-01 17:33:56 +0900

    fix/url url수정

--- gathering/urls.py
+++ gathering/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from django.views.generic import TemplateView 
+from django.views.generic import TemplateView
 from . import views
 
 app_name = "gathering"
@@ -13,23 +13,20 @@ urlpatterns = [
     # # path('<int:gathering_id>/delete', views.GatheringDeleteView.as_view(), name='gathering-delete'),
     # # 투표
     # path('<int:gathering_id>/create', views.PollCreateView.as_view(), name='poll-create'),
-
     # # path('<int:gathering_id>/create', views.poll_add, name='poll-create'),
     # # # 댓글
     # # path('<int:gathering_id>/comments/create', views.CommentCreateView.as_view(), name='comment-create'),
-    path('list/', views.polls_list, name='gathering-list'),
-    path('list/user/', views.list_by_user, name='list_by_user'),
-    path('add/', views.polls_add, name='add'),
-    path('edit/<int:poll_id>/', views.polls_edit, name='edit'),
-    path('delete/<int:poll_id>/', views.polls_delete, name='delete_poll'),
-    path('end/<int:poll_id>/', views.endpoll, name='end_poll'),
-    path('edit/<int:poll_id>/choice/add/', views.add_choice, name='add_choice'),
-    path('edit/choice/<int:choice_id>/', views.choice_edit, name='choice_edit'),
-    path('delete/choice/<int:choice_id>/',
-         views.choice_delete, name='choice_delete'),
-    path('<int:poll_id>/', views.poll_detail, name='detail'),
-    path('<int:poll_id>/vote/', views.poll_vote, name='vote'),
-
+    path("list/", views.polls_list, name="gathering-list"),
+    path("list/user/", views.list_by_user, name="list_by_user"),
+    path("add/", views.polls_add, name="add"),
+    path("edit/<int:poll_id>/", views.polls_edit, name="edit"),
+    path("delete/<int:poll_id>/", views.polls_delete, name="delete_poll"),
+    path("end/<int:poll_id>/", views.endpoll, name="end_poll"),
+    path("edit/<int:poll_id>/choice/add/", views.add_choice, name="add_choice"),
+    path("edit/choice/<int:choice_id>/", views.choice_edit, name="choice_edit"),
+    path("delete/choice/<int:choice_id>/", views.choice_delete, name="choice_delete"),
+    path("<int:poll_id>/", views.poll_detail, name="detail"),
+    path("<int:poll_id>/vote/", views.poll_vote, name="vote"),
     # 댓글
     # 댓글
     path("<int:poll_id>/comments/", views.comment_create, name="comment_create"),
@@ -49,10 +46,7 @@ urlpatterns = [
         name="comment_update_complete",
     ),
     path("<int:poll_id>/like/", views.like, name="like"),
-
-    path('<int:gathering_id>/create', views.poll_add, name='poll-create'),
-    path('meeting_offline/', views.meeting_offline, name='meeting_offline'),
+    path("meeting_offline/", views.meeting_offline, name="meeting_offline"),
     # # 댓글
     # path('<int:gathering_id>/comments/create', views.CommentCreateView.as_view(), name='comment-create'),
-
 ]
