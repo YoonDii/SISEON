50f0440109cd3ad982a49022d9073d7b304277e0
kleenex1 <curian48@naver.com>
2022-12-02 11:05:13 +0900

    feat/gathering url 네이밍 및 로직 수정

--- gathering/forms.py
+++ gathering/forms.py
@@ -1,8 +1,8 @@
 from django import forms 
-from gathering.models import Poll, Choice, GatheringComment
+from gathering.models import Gathering, Choice, GatheringComment
 
 
-class PollAddForm(forms.ModelForm):
+class GatheringAddForm(forms.ModelForm):
 
     choice1 = forms.CharField(label='Choice 1', max_length=100, min_length=1,
                               widget=forms.TextInput(attrs={'class': 'form-control'}))
@@ -10,16 +10,16 @@ class PollAddForm(forms.ModelForm):
                               widget=forms.TextInput(attrs={'class': 'form-control'}))
 
     class Meta:
-        model = Poll
+        model = Gathering
         fields = ['title','category','content', 'choice1', 'choice2', ]
         widgets = {
             'title': forms.Textarea(attrs={'class': 'form-control', 'rows': 5, 'cols': 20}),
         }
 
 
-class EditPollForm(forms.ModelForm):
+class EditGatheringForm(forms.ModelForm):
     class Meta:
-        model = Poll
+        model = Gathering
         fields = ['content', ]
         widgets = {
             'title': forms.Textarea(attrs={'class': 'form-control', 'rows': 5, 'cols': 20}),
