ad10a3b67c754b8a0140b6f420700f6e557a9100
YoonDii <dbswl9506@naver.com>
2022-12-02 17:54:11 +0900

    feat/notices:create,update

---
+++ notices/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+from .models import Notices, Photo
+from django.forms import ClearableFileInput
+
+
+class NoticesForm(forms.ModelForm):
+    class Meta:
+        model = Notices
+        fields = [
+            "title",
+            "content",
+        ]
+        widgets = {
+            "content": forms.Textarea(attrs={"class": "form-control", "rows": 10}),
+        }
+
+
+class PhotoForm(forms.ModelForm):
+    class Meta:
+        model = Photo
+        fields = [
+            "image",
+        ]
+        widgets = {
+            "image": ClearableFileInput(attrs={"multiple": True}),
+        }
+        
