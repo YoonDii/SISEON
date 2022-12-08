0fdf877dde4e79c6513535c5d9856db7ccb97e42
kleenex1 <curian48@naver.com>
2022-12-08 15:29:15 +0900

    배포제발

--- SS/settings.py
+++ SS/settings.py
@@ -201,7 +201,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = "staticfiles"
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
